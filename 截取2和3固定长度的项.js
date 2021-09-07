6a9fdf82abc54795afde040fb5ef01ff7d30b199
xxpzeng <7450765+xxpzeng@user.noreply.gitee.com>
2021-09-07 22:03:57 +0800

    截取数组

---
+++ 截取2和3固定长度的项.js
@@ -0,0 +1,16 @@
+const arr = ['a', 'bbb', 'ccc', 'ddd', 'ggg', 'fff', 'kkk', 'lll',111,333,444,555]
+
+let newListArr1 = [],newListArr2 = []
+let num = 3
+    for(let i = 0;i<arr.length;i+=5){
+        newListArr1.push(arr.slice(i,i+5))
+    }
+console.log(newListArr1)
+
+for(let i = 0;i<newListArr1.length;i++){
+  console.log('循环',newListArr1[i])
+  for(let j= 0;j<newListArr1[i].length;j+=3){
+    newListArr2.push(newListArr1[i].slice(j,j+3))
+  }
+}
+console.log(222,newListArr2)
