a5ed8e7670441c719c6dbfce25583ddd2098a352
xxpzeng <7450765+xxpzeng@user.noreply.gitee.com>
2021-09-07 22:08:11 +0800

    截取数组

--- 截取2和3固定长度的项.js
+++ 截取2和3固定长度的项.js
@@ -7,10 +7,15 @@ let num = 3
     }
 console.log(newListArr1)
 
-for(let i = 0;i<newListArr1.length;i++){
-  console.log('循环',newListArr1[i])
-  for(let j= 0;j<newListArr1[i].length;j+=3){
-    newListArr2.push(newListArr1[i].slice(j,j+3))
+// for(let i = 0;i<newListArr1.length;i++){
+//   console.log('循环',newListArr1[i])
+//   for(let j= 0;j<newListArr1[i].length;j+=3){
+//     newListArr2.push(newListArr1[i].slice(j,j+3))
+//   }
+// }
+newListArr1.map ((item) => {
+  for(let j= 0;j<item.length;j+=3){
+    newListArr2.push(item.slice(j,j+3))
   }
-}
+})
 console.log(222,newListArr2)
