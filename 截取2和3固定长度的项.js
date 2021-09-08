50b56ee469927a1f162d3021461e167427154893
pengzheng <pengzheng01@henhaoji.com>
2021-09-08 14:44:21 +0800

    减少时间复杂度

--- 截取2和3固定长度的项.js
+++ 截取2和3固定长度的项.js
@@ -1,21 +1,30 @@
-const arr = ['a', 'bbb', 'ccc', 'ddd', 'ggg', 'fff', 'kkk', 'lll',111,333,444,555]
+const arr = ['a', 'bbb', 'ccc', 'ddd', 'ggg', 'fff', 'kkk', 'lll',111,333,444,555,222,2]
 
 let newListArr1 = [],newListArr2 = []
-let num = 3
-    for(let i = 0;i<arr.length;i+=5){
-        newListArr1.push(arr.slice(i,i+5))
-    }
-console.log(newListArr1)
-
-// for(let i = 0;i<newListArr1.length;i++){
-//   console.log('循环',newListArr1[i])
-//   for(let j= 0;j<newListArr1[i].length;j+=3){
-//     newListArr2.push(newListArr1[i].slice(j,j+3))
+// 多存循环
+// let num = 3
+//     for(let i = 0;i<arr.length;i+=5){
+//         newListArr1.push(arr.slice(i,i+5))
+//     }
+// console.log(newListArr1)
+// newListArr1.map ((item) => {
+//   for(let j= 0;j<item.length;j+=3){
+//     newListArr2.push(item.slice(j,j+3))
 //   }
-// }
-newListArr1.map ((item) => {
-  for(let j= 0;j<item.length;j+=3){
-    newListArr2.push(item.slice(j,j+3))
-  }
-})
-console.log(222,newListArr2)
+// })
+
+
+
+// 一次while循环
+let i = 0
+    while (arr.length>0) {
+      if(i%2 === 0 ) {
+        newListArr1.push(arr.splice(0,2))
+        console.log('偶数循环',newListArr1,arr)
+      }else {
+        newListArr1.push(arr.splice(0,3))
+        console.log('奇数数循环',newListArr1,arr)
+      }
+      i++
+    }
+console.log(222,newListArr1)
