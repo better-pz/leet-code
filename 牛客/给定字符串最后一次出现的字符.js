7bc7fc9f68e80aaeaf04b8b350663c43cedf2b76
pengzheng <pengzheng01@henhaoji.com>
2021-09-15 21:01:26 +0800

    整理代码

--- 牛客/给定字符串最后一次出现的字符.js
+++ 牛客/给定字符串最后一次出现的字符.js
@@ -1,34 +1,36 @@
 // 给定字符串，请帮小虾米找出最后一个只出现一次的字符。
+function lastUniqueChar( str ) {
+  if(!str) return "";
+  //uniques数组按序存放str中只出现一次的字符
+  var uniques=[];
+  var strLen=str.length;
+  for(let i=0;i<strLen;i++){
+    // lastIndexOf() 方法可返回一个指定的字符串值最后出现的位置，在一个字符串中的指定位置从后向前搜索。
+    // indexOf() 方法可返回某个指定的字符串值在字符串中首次出现的位置。
+      if(str.indexOf(str[i])===str.lastIndexOf(str[i])){
+          uniques.push(str[i]);
+      }
+  }
+  //一定不要漏掉全都是重复出现的字符的情况
+  if(uniques.length===0)
+      return "";
+  else
+      return uniques[uniques.length-1];
+}
 // function lastUniqueChar( str ) {
-//   if(!str) return "";
-//   //uniques数组按序存放str中只出现一次的字符
-//   var uniques=[];
-//   var strLen=str.length;
-//   for(let i=0;i<strLen;i++){
-//       if(str.indexOf(str[i])===str.lastIndexOf(str[i])){
-//           uniques.push(str[i]);
+//   // write code here
+//   if(str == "") {
+//       return ""
+//   }
+//   for(let i = str.length - 1; i >= 0; i--) {
+//       let times = 0;
+//       for(let j = 0; j < str.length; j++) {
+//           times = str[i] == str[j] ? times + 1 : times;
+//       }
+//       if(times == 1) {
+//           return str[i];
 //       }
 //   }
-//   //一定不要漏掉全都是重复出现的字符的情况
-//   if(uniques.length===0)
-//       return "";
-//   else
-//       return uniques[uniques.length-1];
+//   return "";
 // }
-function lastUniqueChar( str ) {
-  // write code here
-  if(str == "") {
-      return ""
-  }
-  for(let i = str.length - 1; i >= 0; i--) {
-      let times = 0;
-      for(let j = 0; j < str.length; j++) {
-          times = str[i] == str[j] ? times + 1 : times;
-      }
-      if(times == 1) {
-          return str[i];
-      }
-  }
-  return "";
-}
 console.log(`结果`, lastUniqueChar('小小下m欧1111')) 
