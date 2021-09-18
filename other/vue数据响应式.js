ad015806e1738b7c96c70c66e3c28f19c84103c9
pengzheng <pengzheng01@henhaoji.com>
2021-09-18 21:04:21 +0800

    vue数据响应式.js

--- other/vue数据响应式.js
+++ other/vue数据响应式.js
@@ -1,6 +1,6 @@
 // defineProperty
 function defineReactive(data) {
-  if (!data || Object.prototype.toString.call(data) !== '[object Object]')
+  if (!data || Object.prototype.toString.call(data) !== "[object Object]")
     return;
   for (let key in data) {
     let val = data[key];
@@ -27,14 +27,62 @@ function track(val, key) {
   console.log("sue set", val, key);
 }
 
-
 const data = {
-  name:'better',
-  firends:['1','2']
-}
-defineReactive(data)
-console.log(data.name)
-console.log(data.firends[1])
-console.log(data.firends[0])
-console.log(Object.prototype.toString.call(data))
+  name: "better",
+  firends: ["1", "2"],
+};
+defineReactive(data);
+console.log(data.name);
+console.log(data.firends[1]);
+console.log(data.firends[0]);
+console.log(Object.prototype.toString.call(data));
 // vue3 proxy
+// const dinner = {
+//   meal:'111'
+// }
+
+// const handler = {
+//   get(target, prop) {
+//     console.log('get...', prop)
+//     return Reflect.get(...arguments)
+//   },
+//   set(target, key, value) {
+//     console.log('get...', prop)
+//     console.log('set',key,value)
+//     return Reflect.set(...arguments)
+//   }
+// }
+// const proxy = new Proxy(dinner, handler)
+// console.log(proxy.meal)
+
+function reactive(obj) {
+  const handler = {
+    get(target, prop, receiver) {
+      track(target, prop);
+      const value =  Reflect.get(...arguments);
+      if(typeof value === 'Object') {
+        reactive(value)
+      }else {
+        return value
+      }
+    },
+    set(target,key, value, receiver) {
+      trigger(target,key, value);
+      return Reflect.set(...arguments);
+    },
+  };
+  return new Proxy(obj,handler)
+}
+function track(data, key) {
+  console.log("sue set", data, key);
+}
+function trigger(data, key,value) {
+  console.log("sue set", key,':',value);
+}
+const dinner = {
+  name:'haochi1'
+}
+const proxy  =reactive(dinner)
+proxy.name
+proxy.list = []
+proxy.list.push(1)
