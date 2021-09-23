3ce04036514c62638c1c196dd599fe65b1e801c1
pengzheng <pengzheng01@henhaoji.com>
2021-09-23 19:45:37 +0800

    Map数组交集

--- leetcode/第349题-两个数组的交集.js
+++ leetcode/第349题-两个数组的交集.js
@@ -7,7 +7,20 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-
   // new Set(nums1) 去重
   return  [...new Set(nums1)].filter(item => nums2.includes(item))
+
+  // 用字典建立一个映射关系用来记录nums1里有的值
+  // const map = new Map
+  // for (let item of nums1) {
+  //   map.set(item,true)
+  // }
+  // const res = []
+  // for(let item2 of nums2) {
+  //   if (map.get(item2)) {
+  //     res.push(item2)
+  //     map.delete(item2)
+  //   }
+  // }
+  // return res
 };
