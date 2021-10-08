4937404eda2a776cc914c3e32dc3d40cd074e8de
Better <pzeng08@163.com>
2021-10-08 20:59:30 +0800

    call-apply-bind.js

---
+++ js手写/call-apply-bind.js
@@ -0,0 +1,57 @@
+Function.prototype.myCall = function (context, ...arr) {
+  console.log(...arr)
+  if (context === null || context === undefined) {
+    context = window
+  }else {
+    context= Object(context)
+  }
+  const specialPrototype = Symbol('特殊属性symbol')
+  context[specialPrototype] = this
+  let result = context[specialPrototype](...arr)
+  delete context[specialPrototype]
+  return result
+} 
+
+Function.prototype.myApply = function (context,arr) {
+  console.log(this)
+  if (context === null || context === undefined) {
+    context = window
+  }else {
+    context= Object(context)
+  }
+  const specialPrototype = Symbol('特殊属性symbol')
+  context[specialPrototype] = this
+  let result = context[specialPrototype](...arr)
+  delete context[specialPrototype]
+  return result
+} 
+
+Function.prototype.myBind = function (objThis, ...params) {
+  const thisFn = this; // 存储源函数以及上方的params(函数参数)
+  // 对返回的函数 secondParams 二次传参
+  let fToBind = function (...secondParams) {
+      const isNew = this instanceof fToBind // this是否是fToBind的实例 也就是返回的fToBind是否通过new调用
+      const context = isNew ? this : Object(objThis) // new调用就绑定到this上,否则就绑定到传入的objThis上
+      return thisFn.call(context, ...params, ...secondParams); // 用call调用源函数绑定this的指向并传递参数,返回执行结果
+  };
+  if (thisFn.prototype) {
+      // 复制源函数的prototype给fToBind 一些情况下函数没有prototype，比如箭头函数
+      fToBind.prototype = Object.create(thisFn.prototype);
+  }
+  return fToBind; // 返回拷贝的函数
+};
+
+let Person = {
+  name: 'Tom',
+  say(name1,name2) {
+      console.log(this)
+      console.log(`我叫${this.name}参数获取的name${name1}和${name2}`)
+  }
+}
+
+
+Person1 = {
+  name: 'Tom1'
+}
+Person.say.myCall(Person1,'jerry1','jerry2')
+Person.say.myApply(Person1,['jerry1','jerry2'])
