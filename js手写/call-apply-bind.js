243e9e625dd6d42a52ce6d2e80814f1821f279cf
Better <pzeng08@163.com>
2021-10-09 20:02:23 +0800

    call-apply-bind.js

--- js手写/call-apply-bind.js
+++ js手写/call-apply-bind.js
@@ -1,5 +1,6 @@
 Function.prototype.myCall = function (context, ...arr) {
-  console.log(...arr)
+  
+  console.log('调用mycall中的this',this)
   if (context === null || context === undefined) {
     context = window
   }else {
@@ -26,21 +27,32 @@ Function.prototype.myApply = function (context,arr) {
   return result
 } 
 
-Function.prototype.myBind = function (objThis, ...params) {
-  const thisFn = this; // 存储源函数以及上方的params(函数参数)
-  // 对返回的函数 secondParams 二次传参
-  let fToBind = function (...secondParams) {
-      const isNew = this instanceof fToBind // this是否是fToBind的实例 也就是返回的fToBind是否通过new调用
-      const context = isNew ? this : Object(objThis) // new调用就绑定到this上,否则就绑定到传入的objThis上
-      return thisFn.call(context, ...params, ...secondParams); // 用call调用源函数绑定this的指向并传递参数,返回执行结果
-  };
-  if (thisFn.prototype) {
-      // 复制源函数的prototype给fToBind 一些情况下函数没有prototype，比如箭头函数
-      fToBind.prototype = Object.create(thisFn.prototype);
+// bind函数的三个特点
+// 1. 改变this指向
+// 2. 第一个参数是this,后面的参数是函数接受参数的值
+// 3. 返回值不变
+function text (a,b,c) {
+  console.log(a,b,c)
+  console.log('this',this)
+  return '我是better'
+}
+Function.prototype.mybind = function() {
+  // 截取第一个参数
+  const argsArr =[...arguments] 
+  const args = argsArr.shift()
+  
+  console.log('第一个参数',args)
+  const self = this
+  return function () {
+    return self.apply (args,argsArr)
   }
-  return fToBind; // 返回拷贝的函数
-};
+}
+const result = text(1,10,100)
+const bindResult = text.mybind({name:'better'},7,77,777)()
+console.log('手写bind',result,bindResult)
 
+
+// call apply测试数据
 let Person = {
   name: 'Tom',
   say(name1,name2) {
