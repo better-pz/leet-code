319ae3dcc73e443a6ef432ae51b798e9309c35c7
Better <pzeng08@163.com>
2021-10-11 20:54:10 +0800

    节流函数封装

--- js手写/防抖&节流.js
+++ js手写/防抖&节流.js
@@ -1,22 +1,47 @@
 function debounce(handle, delay, immediate) {
   let timer = null;
-  return function () {
-    timer && clearTimeout(timer)
-    const self = this
+  return function() {
+    timer && clearTimeout(timer);
+    const self = this;
     const args = [...arguments];
     // 需要立即执行
     if (immediate) {
       const callNow = !timer;
       timer = setTimeout(() => {
-        handle.call(self, ...args)
-        timer = null // 定时将timer清空为null 下次触发时callNow为true可以直接理解执行handle
-      }, delay)
-      if (callNow) handle.call(self, ...args)
-
+        handle.call(self, ...args);
+        timer = null; // 定时将timer清空为null 下次触发时callNow为true可以直接理解执行handle
+      }, delay);
+      if (callNow) handle.call(self, ...args);
     } else {
       timer = setTimeout(() => {
-        handle.call(self, ...args)
-      }, delay)
+        handle.call(self, ...args);
+      }, delay);
     }
+  };
+}
+
+function throttle(handle, delay, immediate) {
+  if (immediate) {
+    let previous = 0;
+    return function() {
+      let now = Date.now();
+      const self = this;
+      const args = [...arguments];
+      if (!previous || now - previous >= delay) {
+        handle.call(self, ...args);
+        previous = now;
+      }
+    };
+  } else {
+    let timer;
+    return function() {
+      const self = this;
+      const args = [...arguments];
+      if (timer) return;
+      timer = setTimeout(() => {
+        handle.call(self, ...args);
+        timer = null;
+      }, delay);
+    };
   }
 }
