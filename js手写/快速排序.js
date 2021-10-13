28f238a08ad219366d4f77339920de5e10e3f1de
Better <pzeng08@163.com>
2021-10-13 11:31:29 +0800

    快速排序.js

--- js手写/快速排序.js
+++ js手写/快速排序.js
@@ -1,4 +1,3 @@
-
 // const quickSort = (array) => {
 //   const length = array.length
 
@@ -35,22 +34,22 @@ const partition = (arr, left, right) => {
   let i = left;
   let j = right;
 
-  while(i < j) {
+  while (i < j) {
     // 先从后往前找小的, 没找到继续找
-    while(i < j && arr[j] > x) {
+    while (i < j && arr[j] > x) {
       j--;
     }
     // 找到了，将值填入坑里, a[j]又变成了坑
-    if(i < j) {
+    if (i < j) {
       a[i] = a[j];
     }
 
     // 然后从前往后找大的，没找到继续找
-    while(i < j && arr[i] < x) {
+    while (i < j && arr[i] < x) {
       i++;
     }
     // 找到了，将值填入之前的坑里
-    if(i < j) {
+    if (i < j) {
       a[j] = a[i];
     }
   }
@@ -59,25 +58,24 @@ const partition = (arr, left, right) => {
   a[i] = x;
 
   return i;
-}
+};
 
 const quickSort = (arr, left, right) => {
   const length = arr.length;
   const start = left || 0;
   const end = right !== undefined ? right : length - 1;
 
-  if(start < end) {
+  if (start < end) {
     const index = partition(arr, start, end);
     quickSort(arr, start, index - 1); // 调整基准值左边
     quickSort(arr, index + 1, end); // 调整基准值右边
   }
 
   return arr;
-}
+};
 
 const a = [2, 1, 3, 6, 4, 5, 9, 8, 7];
 
 // 测试下
 let result = quickSort(a);
-console.log(result);   // [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+console.log(result); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
