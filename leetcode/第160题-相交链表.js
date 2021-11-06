b5646e4d4fd254bc38c31016f473bc4f9f347ea3
Better <pzeng08@163.com>
2021-11-06 13:57:45 +0800

    相交链表

---
+++ leetcode/第160题-相交链表.js
@@ -0,0 +1,18 @@
+var getIntersectionNode = function(headA, headB) {
+  let n1 = headA
+  let n2 = headB
+  while (n1 !== n2) {
+      if(n1 == null) {
+          n1 = headB
+      }else {
+          n1 = n1.next
+      }
+      if(n2 == null) {
+          n2 = headA
+      }else {
+          n2 = n2.next
+      }
+   }
+   return n1
+
+};
