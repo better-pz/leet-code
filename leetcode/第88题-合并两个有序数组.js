ec13632593d0b4606a45fefa17a602fc65475aa9
Better <pzeng08@163.com>
2021-11-06 14:02:55 +0800

    合并两个有序数组

--- leetcode/第88题-合并两个有序数组.js
+++ leetcode/第88题-合并两个有序数组.js
@@ -1,6 +1,5 @@
 var merge = function(nums1, m, nums2, n) {
  nums1.splice(m,nums1.length-m,...nums2)
-
   return nums1.sort((a,b)=> a-b)
 };
 let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
