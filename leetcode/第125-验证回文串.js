e0ebef94a42fa820461c6ff5ca9fba611264ab55
Better <pzeng08@163.com>
2021-11-13 14:31:10 +0800

    验证回文串

---
+++ leetcode/第125-验证回文串.js
@@ -0,0 +1,15 @@
+var isPalindrome = function(s) {
+  s = s.toLocaleLowerCase().replace(/[\W_]/g,'')
+  if(s.length< 2) return true
+  let left =0,right = s.length-1
+  while(left<right) {
+      if(s[left] !== s[right]) {
+          return false
+
+      }
+      left ++
+      right --
+  }
+  return true
+
+}; 
