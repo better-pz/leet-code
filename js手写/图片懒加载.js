280a31f614b2d7a58ebcb75d272e938819aa7862
Better <pzeng08@163.com>
2022-03-14 20:12:25 +0800

    图片懒加载.js

--- js手写/图片懒加载.js
+++ js手写/图片懒加载.js
@@ -8,10 +8,14 @@ const imgLazyload = (function() {
     imgList.forEach((img,index) => {
       let rec = img.getBoundingClientRect()
       if(rec.top < window.innerHeight + 100) {
-        
+        data.src = img.dataset.src
+        deleteIndexList.push(index)
+        count++
+        if(count === length) document.removeEventListener('scroll',imgLazyload)
 
       }
     })
+    imgList = imgList.filter((img,index) => !deleteIndexList.includes(index))
   }
 
 
