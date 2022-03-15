bd34e7ae45a3a3240672cacade699ae606ee843a
Better <pzeng08@163.com>
2022-03-15 20:39:44 +0800

    手写promise.all

--- js手写/promise-all.js
+++ js手写/promise-all.js
@@ -1,8 +1,14 @@
 Promise.all = function(arr) {
   let index = 0,result =[]
   return new Promise ((resolve,reject) => {
-    arr.forEach((item,index) => {
-      
+    arr.forEach((item,i) => {
+      Promise.resolve(item).then(val => {
+        index++
+        result[i] = val
+        if(index === arr.length) {
+          resolve(result)
+        }
+      }).catch(error => reject(error))
     })
   })
 } 
