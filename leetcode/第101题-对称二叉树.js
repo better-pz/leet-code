9772e1902ef5ae59feb318eff0d4fd5dff18912c
Better <pzeng08@163.com>
2022-04-18 21:02:30 +0800

    对称二叉树

--- leetcode/第101题-对称二叉树.js
+++ leetcode/第101题-对称二叉树.js
@@ -1,22 +1,48 @@
-function isSymmetric(root) {
-  if (!root) return true;
-  const isMirror = (l, r) => {
-      const queue = [l, r];
-      while (queue.length) {
-          const u = queue.shift();
-          const v = queue.shift();
-          if (u == null && v == null) continue;
-          if (u == null || v == null) {
-              return false;
-          }
-          if (u.val !== v.val) {
-              return false;
-          }
-          queue.push(u.left, v.right);
-          queue.push(u.right, v.left);
-      }
-      return true;
-  }
-  return isMirror(root.left, root.right)
-};
+// function isSymmetric(root) {
+//   if (!root) return true;
+//   const isMirror = (l, r) => {
+//       const queue = [l, r];
+//       while (queue.length) {
+//           const u = queue.shift();
+//           const v = queue.shift();
+//           if (u == null && v == null) continue;
+//           if (u == null || v == null) {
+//               return false;
+//           }
+//           if (u.val !== v.val) {
+//               return false;
+//           }
+//           queue.push(u.left, v.right);
+//           queue.push(u.right, v.left);
+//       }
+//       return true;
+//   }
+//   return isMirror(root.left, root.right)
+// };
 
+var isSymmetric = function(root) {
+    //迭代方法判断是否是对称二叉树
+    //首先判断root是否为空
+    if(root===null){
+        return true;
+    }
+    let queue=[];
+    queue.push(root.left);
+    queue.push(root.right);
+    while(queue.length){
+        let leftNode=queue.shift();//左节点
+        let rightNode=queue.shift();//右节点
+        if(leftNode===null&&rightNode===null){
+            continue;
+        }
+        if(leftNode===null||rightNode===null||leftNode.val!==rightNode.val){
+            return false;
+        }
+        queue.push(leftNode.left);//左节点左孩子入队
+        queue.push(rightNode.right);//右节点右孩子入队
+        queue.push(leftNode.right);//左节点右孩子入队
+        queue.push(rightNode.left);//右节点左孩子入队
+    }
+    return true;
+  };
+  
