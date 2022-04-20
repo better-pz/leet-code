ced0999bdbcd26200b7a7917f5ad935305a0deab
Better <pzeng08@163.com>
2022-04-20 21:05:31 +0800

    注释

--- leetcode/第226题-翻转二叉树.js
+++ leetcode/第226题-翻转二叉树.js
@@ -11,16 +11,22 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
+
   var invertTree = function(root) {
     let now = [root]
+    // 广度优先遍历,利用层次遍历
+    // 设置当前存储节点的数组,初始化是根节点
+    
     while (now.length) {
         const next = []
         now.forEach(item => {
+            // 节点为null直接返回
             if (item === null) return
+            // 定义第三变量交换左右子节点
             let n = item.left
             item.left = item.right
             item.right = n
+            // 将左右子节点放进数组
             next.push(item.left)
             next.push(item.right)
         })
@@ -29,4 +35,3 @@ var invertTree = function(root) {
     return root
 };
 
-};
