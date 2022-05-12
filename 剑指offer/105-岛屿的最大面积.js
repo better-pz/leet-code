3ee75991c9ee93f5c69b572c299c5c88c5bae002
Better <pzeng08@163.com>
2022-05-12 11:51:06 +0800

    修正

--- 剑指offer/105-岛屿的最大面积.js
+++ 剑指offer/105-岛屿的最大面积.js
@@ -3,22 +3,34 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-  let result = 0
-  for (let i in grid) {
-      for(let j in grid[i] ) {
-          if(grid[i][j] === 1) {
-              console.log(getArea(i,j,0))
-             result = Math.max(result, getArea(i,j,0))
-          }
-      }
-  }
-  return  result
-  function getArea (i,j,area) {
-      console.log(grid)
-      if( i+1< grid.length && grid[i+1][j] === 1) area = getArea(i+1,j,area)
-      if( j+1< grid[i].length && grid[i][j+1] === 1) area= getArea(i,j+1,area)
-      if( i-1> 0 && grid[i+1][j] === 1) area = getArea(i-1,j,area)
-      if( j-1> 0 && grid[i][j-1] === 1) area = getArea(i,j-1,area)
-      return area +1 
-  }
+    let result = 0
+    // for (let i in grid) {
+    //     for(let j in grid[i] ) {
+    //         if(grid[i][j] === 1) {
+            
+    //             const area = getMaxArea(i, j, 0);
+    //            result = Math.max(result, area);
+    //         }
+    //     }
+    // }
+        for(let i = 0; i < grid.length; i++) {
+            for(let j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    const area = getMaxArea(i, j, 0);
+                    result = Math.max(result, area);
+                }
+            }
+        }
+        
+    function getMaxArea (i,j,area) {
+        // console.log(grid)
+        grid[i][j] = 0
+        if( i+1< grid.length && grid[i+1][j] === 1) area = getMaxArea(i+1,j,area)
+        if( j+1< grid[i].length && grid[i][j+1] === 1) area= getMaxArea(i,j+1,area)
+        if( i-1>= 0 && grid[i-1][j] === 1) area = getMaxArea(i-1,j,area)
+        if( j-1>= 0 && grid[i][j-1] === 1) area = getMaxArea(i,j-1,area)
+        return area +1 
+    }
+    
+    return  result
 };
