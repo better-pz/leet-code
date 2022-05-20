74e7014158b3d2a05a3103c15b02286c9fbebd4c
Better <pzeng08@163.com>
2022-05-20 20:54:04 +0800

    updae

---
+++ leetcode/第516题-最长回文子序列.js
@@ -0,0 +1,10 @@
+
+/**
+ * @param {string} s
+ * @return {number}
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+  子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ */
+var longestPalindromeSubseq = function(s) {
+    
+};
